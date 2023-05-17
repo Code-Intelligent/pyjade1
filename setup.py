015f3cc2ba54a42f6228166038ecb21ff0b0311f
Blange1995 <18664667495@163.com>
2023-05-17 15:35:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup,find_packages
-
-setup(name='pyjade',
-      version='4.0.0',
-      download_url='git@github.com:syrusakbary/pyjade.git',
-      packages=find_packages(),
-      author='Syrus Akbary',
-      author_email='me@syrusakbary.com',
-      description='Jade syntax template adapter for Django, Jinja2, Mako and '
-                  'Tornado templates',
-      long_description=open('README.rst').read(),
-      keywords='jade template converter',
-      url='http://github.com/syrusakbary/pyjade',
-      license='MIT',
-      entry_points={
-          'console_scripts' : ['pyjade = pyjade.convert:convert_file',]
-      },
-      install_requires=['six'],
-      tests_require=[
-            'nose',
-            'django',
-            'jinja2',
-            'tornado',
-            'pyramid >= 1.4, <= 1.4.99',
-            'mako',
-      ])
+# 恶意用户输入 username = "admin' OR 1=1"
+query = "SELECT * FROM users WHERE username = '{0}'".format(username)
+cursor.execute(query)
